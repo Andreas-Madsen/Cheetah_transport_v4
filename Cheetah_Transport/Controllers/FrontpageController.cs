4db99bacc89248c17e85d013734d22c438c983ae
NCLAN\afw <afw@netcompany.com>
2021-05-21 01:55:48 +0200

    Migrate frontend

---
+++ Cheetah_Transport/Controllers/FrontpageController.cs
@@ -0,0 +1,27 @@
+﻿using Cheetah_Transport.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cheetah_Transport.Controllers
+{
+    public class FrontpageController : Controller
+    {
+        private readonly ILogger<FrontpageController> _logger;
+
+        public FrontpageController(ILogger<FrontpageController> logger)
+        {
+            _logger = logger;
+        }
+
+            public ActionResult Start()
+            {
+                return View();
+            }
+        
+    }
+}
---
+++ Cheetah_Transport/Controllers/LocationSelectorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cheetah_Transport.Controllers
+{
+    public class LocationSelectorController : Controller
+    {
+        public IActionResult LocationPage()
+        {
+            return View();
+        }
+    }
+}
