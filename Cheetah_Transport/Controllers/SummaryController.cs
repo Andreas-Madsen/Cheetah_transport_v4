2d03ab6e77c40eff9ebfaeed4a630a6741ad67c3
Frederik Johansen <frederik.lynggaard@icloud.com>
2021-05-21 04:20:04 +0200

    Small improvement to Summary

--- Cheetah_Transport/Controllers/SummaryController.cs
+++ Cheetah_Transport/Controllers/SummaryController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cheetah_Transport.Data;
+using Cheetah_Transport.Models;
 
 namespace Cheetah_Transport.Controllers
 {
@@ -10,6 +12,32 @@ namespace Cheetah_Transport.Controllers
     {
         public IActionResult SummaryPage()
         {
+            var dao = new TransportCenterDAO();
+            var t1 = dao.FetchOne(2);
+            var t2 = dao.FetchOne(23);
+
+            var routeEngine = new RouteEngine.RouteEngine();
+            var res = routeEngine.ComputeRoute(null, t1, t2);
+
+            var distance = res.Item2;
+            var route = res.Item1;
+
+            
+            List<string> cities = new List<string>();
+            foreach (var cityId in route)
+            {
+                var city = dao.FetchOne(cityId);
+                
+                cities.Add(city.Name);
+            }
+
+            ViewBag.From = t1.Name;
+            ViewBag.To = t2.Name;
+            ViewBag.Distance = distance;
+            ViewBag.Route = cities;
+
+
+
             return View();
         }
     }
